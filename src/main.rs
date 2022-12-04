17f02ffd553009f8b1421b36f08ce5790abeceef
Tristan F <LeoDog896@hotmail.com>
2022-12-03 22:05:46 -0500

    basic search system

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+use clap::Parser;
+
+/// Get lyrics from various songs.
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// The query of the song to get lyrics for.
+    song_query: String,
+}
+
 fn fetch_lyrics(url: String) -> String {
     let response = reqwest::blocking::get(url)
-    .expect("Failed to fetch lyrics: Initial request failed")
-    .text()
-    .expect("Failed to fetch lyrics: Request for text timed out");
+        .expect("Failed to fetch lyrics: Initial request failed")
+        .text()
+        .expect("Failed to fetch lyrics: Request for text timed out");
 
     let document = scraper::Html::parse_document(&response);
 
@@ -15,9 +25,35 @@ fn fetch_lyrics(url: String) -> String {
     text_lyrics + "\n"
 }
 
+fn search(query: String) -> String {
+    let url = format!("https://www.musixmatch.com/search/{}", query);
+
+    let response = reqwest::blocking::get(url)
+        .expect("Failed to search: Initial request failed")
+        .text()
+        .expect("Failed to search: Request for text timed out");
+
+    let document = scraper::Html::parse_document(&response);
+
+    let song_selector = scraper::Selector::parse(".title").unwrap();
+
+    let song = document.select(&song_selector).next().unwrap();
+
+    let song_url = song.value().attr("href").unwrap();
+    
+    let song_url = format!("https://www.musixmatch.com{}", song_url);
+
+    song_url
+}
+
 fn main() {
-    let text_lyrics = fetch_lyrics("https://www.musixmatch.com/lyrics/Shawn-Wasabi-feat-Hollis/Otter-Pop".to_string());
+    let args = Args::parse();
+
+    let song_url = search(args.song_query);
+
+    let text_lyrics = fetch_lyrics(
+        song_url,
+    );
 
     println!("{}", text_lyrics + "\n");
-
 }
